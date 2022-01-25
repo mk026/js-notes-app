dc4499d74777ed88622c4a5430938221c8fd38d6
Mikhail <mk0602604@gmail.com>
2022-01-25 19:25:02 +0300

    Configure dev server and html-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,18 @@
 const path = require("path");
 const webpack = require("webpack");
+const HtmlWebpackPlugi = require("html-webpack-plugin");
 
 module.exports = {
   entry: "./src/index.js",
   mode: "development",
   devtool: "source-map",
-  plugins: [new webpack.ProgressPlugin()],
+  devServer: {
+    historyApiFallback: true,
+  },
+  plugins: [
+    new webpack.ProgressPlugin(),
+    new HtmlWebpackPlugi({ template: "./src/template.html" }),
+  ],
   output: {
     filename: "[name].js",
     path: path.resolve(__dirname, "dist"),
