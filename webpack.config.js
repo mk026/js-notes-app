6559b665f22ebb8b2081a04ecf7998d9697466eb
Mikhail <mk0602604@gmail.com>
2022-01-26 15:21:37 +0300

    Install and configure babel

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,31 @@
-const path = require("path");
-const webpack = require("webpack");
-const HtmlWebpackPlugi = require("html-webpack-plugin");
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugi = require('html-webpack-plugin');
 
 module.exports = {
-  entry: "./src/index.js",
-  mode: "development",
-  devtool: "source-map",
+  entry: './src/index.js',
+  mode: 'development',
+  devtool: 'source-map',
   devServer: {
     historyApiFallback: true,
   },
   plugins: [
     new webpack.ProgressPlugin(),
-    new HtmlWebpackPlugi({ template: "./src/template.html" }),
+    new HtmlWebpackPlugi({ template: './src/template.html' }),
   ],
   output: {
-    filename: "[name].js",
-    path: path.resolve(__dirname, "dist"),
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+    ],
   },
 };
