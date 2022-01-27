3ac49ec7a8bda4ac7535d95ff6b087bc770498d3
Mikhail <mk0602604@gmail.com>
2022-01-27 18:27:45 +0300

    Fix webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
   entry: './src/index.js',
@@ -12,6 +13,7 @@ module.exports = {
   plugins: [
     new webpack.ProgressPlugin(),
     new HtmlWebpackPlugin({ template: './src/template.html' }),
+    new MiniCssExtractPlugin(),
   ],
   output: {
     filename: '[name].js',
