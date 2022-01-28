488a230093006c1fbfba06f675e5cc2922494f1d
Mikhail <mk0602604@gmail.com>
2022-01-28 14:43:40 +0300

    Create routes with route constructor

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
 import NavController from './controllers/Nav.controller';
+import HomeController from './controllers/Home.controller';
+import NotesController from './controllers/Notes.controller';
+
 import NavView from './views/Nav.view';
+import HomeView from './views/Home.view';
+import NotesView from './views/Notes.view';
+
+import Route from './router/Route';
 
 const appRoot = document.getElementById('app');
 
+const homeView = new HomeView();
+const notesView = new NotesView();
+
+const homeController = new HomeController(homeView);
+const notesController = new NotesController(notesView);
+
 const routes = [
-  { name: 'Home', path: '/home' },
-  { name: 'Notes', path: '/notes' },
-  { name: 'Projects', path: '/projects' },
+  new Route('Home', '/', homeController),
+  new Route('Notes', '/notes', notesController),
 ];
 
 const navView = new NavView();
