e9ad6388ed501699963d09bbec39a488461f78f6
Mikhail <mk0602604@gmail.com>
2022-02-02 14:38:55 +0300

    Create and render todos route

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import NavController from './controllers/Nav.controller';
 import HomeController from './controllers/Home.controller';
 import NotesController from './controllers/Notes.controller';
+import TodosController from './controllers/Todos.controller';
 
 import NavView from './views/Nav.view';
 import HomeView from './views/Home.view';
 import NotesView from './views/Notes.view';
+import TodosView from './views/Todos.view';
 
 import NotesModel from './models/Notes.model';
 
@@ -19,13 +21,16 @@ const notesModel = new NotesModel(getDummyNotes(6));
 
 const homeView = new HomeView();
 const notesView = new NotesView();
+const todosView = new TodosView();
 
 const homeController = new HomeController(appRoot, homeView);
 const notesController = new NotesController(appRoot, notesView, notesModel);
+const todosController = new TodosController(appRoot, todosView);
 
 const routes = [
   new Route('Home', '/', homeController),
   new Route('Notes', '/notes', notesController),
+  new Route('Todos', '/todos', todosController),
 ];
 
 const router = new Router(routes);
