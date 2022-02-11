0f2cc194a00f864cd1d839d26112cd27f7bfe708
Mikhail <mk0602604@gmail.com>
2022-02-11 12:55:48 +0300

    Add api service

---
+++ src/api.js
@@ -0,0 +1,21 @@
+export default class ApiService {
+  constructor(baseUrl) {
+    this.baseUrl = baseUrl;
+  }
+
+  getNotes() {}
+
+  addNote() {}
+
+  editNote() {}
+
+  removeNote() {}
+
+  getTodos() {}
+
+  addTodo() {}
+
+  editTodo() {}
+
+  removeTodo() {}
+}
