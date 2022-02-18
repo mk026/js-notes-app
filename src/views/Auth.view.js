3c8b3946f002fd6bdd2d83e337bc03d663e9eaa6
Mikhail <mk0602604@gmail.com>
2022-02-18 23:27:56 +0300

    Add signin/signup form templates

--- src/views/Auth.view.js
+++ src/views/Auth.view.js
@@ -13,4 +13,32 @@ export default class AuthView extends AbstractView {
       </div>
     `;
   }
+
+  getSigninForm() {
+    return `
+      <form class="signin-form">
+        <label for="email">Email</label>
+        <input id="email" type="email"/>
+        <label for="password">Password</label>
+        <input id="password" type="password"/>
+        <button type="submit">Sign in</button>
+      </form>
+    `;
+  }
+
+  getSignupForm() {
+    return `
+      <form class="signup-form">
+        <label for="name">Name</label>
+        <input id="name" type="text"/>
+        <label for="email">Email</label>
+        <input id="email" type="email"/>
+        <label for="password">Password</label>
+        <input id="password" type="password"/>
+        <label for="confirm-password">Confirm password</label>
+        <input id="confirm-password" type="password"/>
+        <button type="submit">Sign up</button>
+      </form>
+    `;
+  }
 }
