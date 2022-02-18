887f28698f3e19111bf3cb865f02e1f5c7b1a398
Mikhail <mk0602604@gmail.com>
2022-02-18 23:04:28 +0300

    Create new route for auth page

--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ import NavController from './controllers/Nav.controller';
 import HomeController from './controllers/Home.controller';
 import NotesController from './controllers/Notes.controller';
 import TodosController from './controllers/Todos.controller';
+import AuthController from './controllers/Auth.controller';
 
 import NavView from './views/Nav.view';
 import HomeView from './views/Home.view';
 import NotesView from './views/notes/Notes.view';
 import TodosView from './views/todos/Todos.view';
+import AuthView from './views/Auth.view';
 
 import NotesModel from './models/Notes.model';
 import TodosModel from './models/Todos.model';
@@ -30,15 +32,18 @@ const todosModel = new TodosModel(todosService);
 const homeView = new HomeView();
 const notesView = new NotesView();
 const todosView = new TodosView();
+const authView = new AuthView();
 
 const homeController = new HomeController(appRoot, homeView);
 const notesController = new NotesController(appRoot, notesView, notesModel);
 const todosController = new TodosController(appRoot, todosView, todosModel);
+const authController = new AuthController(appRoot, authView);
 
 const routes = [
   new Route('Home', '/', homeController),
   new Route('Notes', '/notes', notesController),
   new Route('Todos', '/todos', todosController),
+  new Route('Auth', '/auth', authController),
 ];
 
 const router = new Router(routes);
