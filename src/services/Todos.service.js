f0683f073c2669251f9c9978d6cf8d54ca39f891
Mikhail <mk0602604@gmail.com>
2022-02-19 16:04:50 +0300

    Send auth token with all requests in todos service

--- src/services/Todos.service.js
+++ src/services/Todos.service.js
@@ -6,7 +6,10 @@ export default class TodosService {
 
   async getTodos() {
     try {
-      const response = await fetch(`${this.baseUrl}/todos`);
+      const token = `Bearer ${this.authService.getToken()}`;
+      const response = await fetch(`${this.baseUrl}/todos`, {
+        headers: { Authorization: token },
+      });
       const data = await response.json();
       return data.map(({ _id, title, completed }) => ({
         id: `todo_${_id}`,
@@ -20,10 +23,11 @@ export default class TodosService {
 
   async addTodo(todoData) {
     try {
+      const token = `Bearer ${this.authService.getToken()}`;
       const response = await fetch(`${this.baseUrl}/todos`, {
         method: 'POST',
         body: JSON.stringify(todoData),
-        headers: { 'Content-Type': 'application/json' },
+        headers: { 'Content-Type': 'application/json', Authorization: token },
       });
       const data = await response.json();
       const newTodo = { ...data, id: `todo_${data._id}` };
@@ -38,10 +42,11 @@ export default class TodosService {
     const editedTodo = { ...todoData, _id: todoData.id.replace(/^todo_/, '') };
     delete editedTodo.id;
     try {
+      const token = `Bearer ${this.authService.getToken()}`;
       const response = await fetch(`${this.baseUrl}/todos`, {
         method: 'PUT',
         body: JSON.stringify(editedTodo),
-        headers: { 'Content-Type': 'application/json' },
+        headers: { 'Content-Type': 'application/json', Authorization: token },
       });
       const data = await response.json();
       const updatedTodo = { ...data, id: `todo_${data._id}` };
@@ -54,10 +59,12 @@ export default class TodosService {
 
   async removeTodo(id) {
     try {
+      const token = `Bearer ${this.authService.getToken()}`;
       const response = await fetch(
         `${this.baseUrl}/todos/${id.replace(/^todo_/, '')}`,
         {
           method: 'DELETE',
+          headers: { Authorization: token },
         }
       );
       const data = await response.json();
