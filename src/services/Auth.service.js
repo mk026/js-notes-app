5d84466b9a96e5f2f87be24e083cf5b283e73219
Mikhail <mk0602604@gmail.com>
2022-02-19 15:24:12 +0300

    Add signin/signup methods in auth service

--- src/services/Auth.service.js
+++ src/services/Auth.service.js
@@ -1,11 +1,40 @@
 export default class AuthService {
   constructor(baseUrl) {
     this.baseUrl = baseUrl;
+    this.token = null;
   }
 
-  signup() {}
+  setToken(token) {
+    this.token = token;
+  }
 
-  signin() {}
+  async signup(name, email, password) {
+    try {
+      const response = await fetch(`${this.baseUrl}/signup`, {
+        method: 'POST',
+        body: JSON.stringify({ name, email, password }),
+        headers: { 'Content-Type': 'application/json' },
+      });
+      const data = await response.json();
+      this.setToken(data.token);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async signin(email, password) {
+    try {
+      const response = await fetch(`${this.baseUrl}/signin`, {
+        method: 'POST',
+        body: JSON.stringify({ email, password }),
+        headers: { 'Content-Type': 'application/json' },
+      });
+      const data = await response.json();
+      this.setToken(data.token);
+    } catch (error) {
+      console.log(error);
+    }
+  }
 
   signout() {}
 }
