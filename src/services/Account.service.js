3d5fe8c84e9e3f04a454c99bacadd3b21c991965
Mikhail <mk0602604@gmail.com>
2022-02-23 13:50:58 +0300

    Create account service

---
+++ src/services/Account.service.js
@@ -0,0 +1,16 @@
+export default class AccountService {
+  constructor(baseUrl, authService) {
+    this.baseUrl = baseUrl;
+    this.authService = authService;
+  }
+
+  getName() {}
+
+  getEmail() {}
+
+  changeName() {}
+
+  changeEmail() {}
+
+  changePassword() {}
+}
