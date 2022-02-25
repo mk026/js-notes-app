20c5fd111ced7b85302e57f5de7f8790d65ee04c
Mikhail <mk0602604@gmail.com>
2022-02-25 12:51:46 +0300

    Add change name/email form templates

--- src/views/Account.view.js
+++ src/views/Account.view.js
@@ -14,8 +14,31 @@ export default class AccountView extends AbstractView {
           <h3 class="account__name"></h3>
           <p class="account__email"></p>
         </div>
+        <div class="account__form-container"></div>
         <button id="signout">Signout</button>
       </div>
+    `;
+  }
+
+  getChangeNameFormTemplate() {
+    return `
+      <form id="change-name">
+        <label for="new-name">New name</label>
+        <input id="new-name" type="text"/>
+        <button id="save-new-name" type="submit">Save</button>
+        <button id="cancel-new-name">Cancel</button>
+      </form>
+    `;
+  }
+
+  getChangeEmailFormTemplate() {
+    return `
+      <form id="change-email">
+        <label for="new-email">New email</label>
+        <input id="new-email" type="text"/>
+        <button id="save-new-email" type="submit">Save</button>
+        <button id="cancel-new-email">Cancel</button>
+      </form>
     `;
   }
 
