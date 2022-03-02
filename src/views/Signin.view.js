3c19edef47e8b6f758576251f73a5b763338cb02
Mikhail <mk0602604@gmail.com>
2022-03-02 14:51:44 +0300

    Create separate view for signin form

---
+++ src/views/Signin.view.js
@@ -0,0 +1,47 @@
+import AbstractView from './Abstract.view';
+
+export default class SigninView extends AbstractView {
+  constructor() {
+    super();
+    this.element = this.getElement();
+  }
+
+  getTemplate() {
+    return `
+      <form class="signin-form">
+        <label for="email">Email</label>
+        <input id="email" type="email"/>
+        <label for="password">Password</label>
+        <input id="password" type="password"/>
+        <button type="submit">Sign in</button>
+      </form>
+    `;
+  }
+
+  setOnSignin(handler) {
+    this.onSignin = handler;
+  }
+
+  mount(container) {
+    container.appendChild(this.element, 'afterbegin');
+
+    this.attachSigninHandler();
+  }
+
+  unmount() {
+    this.element.remove();
+  }
+
+  signinHandler = (event) => {
+    event.preventDefault();
+
+    const email = this.element.querySelector('#email').value;
+    const password = this.element.querySelector('#password').value;
+
+    this.onSignin(email, password);
+  };
+
+  attachSigninHandler() {
+    this.element.addEventListener('submit', this.signinHandler);
+  }
+}
