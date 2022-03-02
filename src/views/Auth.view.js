fcbe67980c2693891930b38d2a8d84aef107e734
Mikhail <mk0602604@gmail.com>
2022-03-02 16:21:40 +0300

    Use signin/signup views inside of auth view

--- src/views/Auth.view.js
+++ src/views/Auth.view.js
@@ -1,104 +1,51 @@
 import AbstractView from './Abstract.view';
+import SigninView from './Signin.view';
+import SignupView from './Signup.view';
 
 export default class AuthView extends AbstractView {
   constructor() {
     super();
     this.element = this.getElement();
+    this.signupView = new SignupView();
+    this.signinView = new SigninView();
+  }
+
+  get formContainer() {
+    return this.element.querySelector('.auth__form-container');
   }
 
   getTemplate() {
     return `
       <div class="auth">
         <h2>Auth</h2>
+        <div class="auth__controls">
+          <button id="switch-to-signin">Signin</button>
+          <button id="switch-to-signup">Signup</button>
+        </div>
         <div class="auth__form-container"></div>
       </div>
     `;
   }
 
-  getSigninForm() {
-    return `
-      <form class="signin-form">
-        <label for="email">Email</label>
-        <input id="email" type="email"/>
-        <label for="password">Password</label>
-        <input id="password" type="password"/>
-        <button type="submit">Sign in</button>
-        <button id="switch-to-signup">Switch to signup</button>
-      </form>
-    `;
-  }
-
-  getSignupForm() {
-    return `
-      <form class="signup-form">
-        <label for="name">Name</label>
-        <input id="name" type="text"/>
-        <label for="email">Email</label>
-        <input id="email" type="email"/>
-        <label for="password">Password</label>
-        <input id="password" type="password"/>
-        <label for="confirm-password">Confirm password</label>
-        <input id="confirm-password" type="password"/>
-        <button type="submit">Sign up</button>
-        <button id="switch-to-signin">Switch to signin</button>
-      </form>
-    `;
-  }
-
   setOnSignup(handler) {
-    this.onSignup = handler;
+    this.signupView.setOnSignup(handler);
   }
 
   setOnSignin(handler) {
-    this.onSignin = handler;
+    this.signinView.setOnSignin(handler);
   }
 
   renderSignupForm = () => {
-    this.element.querySelector('.auth__form-container').innerHTML =
-      this.getSignupForm();
+    this.signupView.mount(this.formContainer);
 
-    this.attachSignupHandler();
     this.attachSwitchToSigninHandler();
   };
 
   renderSigninForm = () => {
-    this.element.querySelector('.auth__form-container').innerHTML =
-      this.getSigninForm();
+    this.signinView.mount(this.formContainer);
 
-    this.attachSigninHandler();
     this.attachSwitchToSignupHandler();
   };
-
-  signupHandler = (event) => {
-    event.preventDefault();
-
-    const name = this.element.querySelector('#name').value;
-    const email = this.element.querySelector('#email').value;
-    const password = this.element.querySelector('#password').value;
-
-    this.onSignup(name, email, password);
-  };
-
-  signinHandler = (event) => {
-    event.preventDefault();
-
-    const email = this.element.querySelector('#email').value;
-    const password = this.element.querySelector('#password').value;
-
-    this.onSignin(email, password);
-  };
-
-  attachSignupHandler() {
-    this.element
-      .querySelector('.signup-form')
-      .addEventListener('submit', this.signupHandler);
-  }
-
-  attachSigninHandler() {
-    this.element
-      .querySelector('.signin-form')
-      .addEventListener('submit', this.signinHandler);
-  }
 
   attachSwitchToSignupHandler() {
     this.element
