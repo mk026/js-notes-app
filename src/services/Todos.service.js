1b68240249be5756718331ad09244c95e195fdaf
Mikhail <mk0602604@gmail.com>
2022-03-08 13:12:59 +0300

    Extend todos service from api service

--- src/services/Todos.service.js
+++ src/services/Todos.service.js
@@ -1,78 +1,44 @@
-export default class TodosService {
+import ApiService from './Api.service';
+
+export default class TodosService extends ApiService {
   constructor(baseUrl, authService) {
-    this.baseUrl = baseUrl;
+    super(baseUrl);
     this.authService = authService;
   }
 
   async getTodos() {
-    try {
-      const token = `Bearer ${this.authService.getToken()}`;
-      const response = await fetch(`${this.baseUrl}/todos`, {
-        headers: { Authorization: token },
-      });
-      const data = await response.json();
-      return data.map(({ _id, title, completed }) => ({
-        id: `todo_${_id}`,
-        title,
-        completed,
-      }));
-    } catch (error) {
-      console.log(error);
-    }
+    const token = `Bearer ${this.authService.getToken()}`;
+    const data = await this.get('todos', token);
+    return data.map(({ _id, title, completed }) => ({
+      id: `todo_${_id}`,
+      title,
+      completed,
+    }));
   }
 
   async addTodo(todoData) {
-    try {
-      const token = `Bearer ${this.authService.getToken()}`;
-      const response = await fetch(`${this.baseUrl}/todos`, {
-        method: 'POST',
-        body: JSON.stringify(todoData),
-        headers: { 'Content-Type': 'application/json', Authorization: token },
-      });
-      const data = await response.json();
-      const newTodo = { ...data, id: `todo_${data._id}` };
-      delete newTodo._id;
-      return newTodo;
-    } catch (error) {
-      console.log(error);
-    }
+    const token = `Bearer ${this.authService.getToken()}`;
+    const data = await this.post('todos', todoData, token);
+    const newTodo = { ...data, id: `todo_${data._id}` };
+    delete newTodo._id;
+    return newTodo;
   }
 
   async editTodo(todoData) {
     const editedTodo = { ...todoData, _id: todoData.id.replace(/^todo_/, '') };
     delete editedTodo.id;
-    try {
-      const token = `Bearer ${this.authService.getToken()}`;
-      const response = await fetch(`${this.baseUrl}/todos`, {
-        method: 'PUT',
-        body: JSON.stringify(editedTodo),
-        headers: { 'Content-Type': 'application/json', Authorization: token },
-      });
-      const data = await response.json();
-      const updatedTodo = { ...data, id: `todo_${data._id}` };
-      delete updatedTodo._id;
-      return updatedTodo;
-    } catch (error) {
-      console.log(error);
-    }
+    const token = `Bearer ${this.authService.getToken()}`;
+    const data = await this.put('todos', editedTodo, token);
+    const updatedTodo = { ...data, id: `todo_${data._id}` };
+    delete updatedTodo._id;
+    return updatedTodo;
   }
 
   async removeTodo(id) {
-    try {
-      const token = `Bearer ${this.authService.getToken()}`;
-      const response = await fetch(
-        `${this.baseUrl}/todos/${id.replace(/^todo_/, '')}`,
-        {
-          method: 'DELETE',
-          headers: { Authorization: token },
-        }
-      );
-      const data = await response.json();
-      const deletedTodo = { ...data, id: `todo_${data._id}` };
-      delete deletedTodo._id;
-      return deletedTodo;
-    } catch (error) {
-      console.log(error);
-    }
+    const token = `Bearer ${this.authService.getToken()}`;
+    const data = await this.delete(`todos/${id.replace(/^todo_/, '')}`, token);
+    const deletedTodo = { ...data, id: `todo_${data._id}` };
+    delete deletedTodo._id;
+    return deletedTodo;
   }
 }
