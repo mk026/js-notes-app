c7fc59621c9c83082b2bfa46517159d8701df8d7
Mikhail <mk0602604@gmail.com>
2022-04-25 13:58:41 +0300

    Add nav styles

--- src/views/Nav.view.js
+++ src/views/Nav.view.js
@@ -1,4 +1,5 @@
 import AbstractView from './Abstract.view';
+import classes from '../styles/Nav.module.css';
 
 export default class NavView extends AbstractView {
   constructor() {
@@ -8,29 +9,35 @@ export default class NavView extends AbstractView {
 
   getTemplate() {
     return `
-      <nav class="nav">
-        <ul class="nav__list">
+      <nav class="${classes.nav}">
+        <ul class="${classes.nav__list}">
         </ul>
       </nav>
     `;
   }
 
   getRouteTemplate(route) {
-    return `<li><a href="${route.path}" class="nav__link">${route.name}</a></li>`;
+    return `
+      <li class="${classes.nav__el}">
+        <a href="${route.path}" class="${classes.nav__link}">${route.name}</a>
+      </li>
+    `;
   }
 
   renderRoutes(routes) {
-    const navListContainer = this.element.querySelector('.nav__list');
+    const navListContainer = this.element.querySelector(
+      `.${classes.nav__list}`
+    );
     const navList = routes.map(this.getRouteTemplate);
     navListContainer.innerHTML = navList.join('');
   }
 
   setActiveLink = (href) => {
-    this.element.querySelectorAll('.nav__link').forEach((link) => {
+    this.element.querySelectorAll(`.${classes.nav__link}`).forEach((link) => {
       if (link.href == href) {
-        link.classList.add('active');
+        link.classList.add(classes.active);
       } else {
-        link.classList.remove('active');
+        link.classList.remove(classes.active);
       }
     });
   };
